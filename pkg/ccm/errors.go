8497bfec0efed6074e1d33f1c035ea2ce9cf0395
Alejandro Mery <amery@jpi.io>
2023-09-17 23:47:51 +0000

    ccm: introduce ErrNotImplemented, alias of cloudprovider.NotImplemented
    Signed-off-by: Alejandro Mery <amery@jpi.io>

---
+++ pkg/ccm/errors.go
@@ -0,0 +1,10 @@
+package ccm
+
+import (
+	cloudprovider "k8s.io/cloud-provider"
+)
+
+var (
+	// ErrNotImplemented indicates something isn't implemented yet
+	ErrNotImplemented = cloudprovider.NotImplemented
+)
