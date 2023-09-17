3e94a6314ba77443f4250ed11aa139dc04e76b14
Alejandro Mery <amery@jpi.io>
2023-09-17 19:23:41 +0000

    darvaza-kube-ccm: enable json logs and add clientgo prometheus metrics
    Signed-off-by: Alejandro Mery <amery@jpi.io>

--- cmd/darvaza-kube-ccm/main.go
+++ cmd/darvaza-kube-ccm/main.go
@@ -5,6 +5,9 @@ import (
 	"os"
 
 	"k8s.io/component-base/cli"
+	_ "k8s.io/component-base/logs/json/register"          // register optional JSON log format
+	_ "k8s.io/component-base/metrics/prometheus/clientgo" // load all the prometheus client-go plugins
+	_ "k8s.io/component-base/metrics/prometheus/version"  // for version metric registration
 	"k8s.io/klog/v2"
 
 	"darvaza.org/kube/pkg/ccm"
